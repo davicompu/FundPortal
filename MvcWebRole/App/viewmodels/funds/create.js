a6b1847f15efdbffacbc3801eb5457554a506695
chrisjsherm <chris.sherman@outlook.com>
2013-12-09 14:51:19 -0500

    Compile changes while trying to fix 'n is undefined' error when main-built.js is used in Release mode. Still receiving this error.

--- MvcWebRole/App/viewmodels/funds/create.js
+++ MvcWebRole/App/viewmodels/funds/create.js
@@ -27,7 +27,12 @@
             logger.log('Create fund view activated', null, 'funds/create', false);
 
             vm.item(datacontext.createItem({
-                Number: 'temporary',
+                Number: 'temporary-' +
+                    'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+                        /[xy]/g, function (c) {
+                            var r = Math.random() * 16 | 0, v = c == 'x' ? r : r & 0x3 | 0x8;
+                            return v.toString(16);
+                        }),
                 AreaId: queryString.areaid
             }));
 
